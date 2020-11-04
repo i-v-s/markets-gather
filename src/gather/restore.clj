cbb81f6c04dfb142716f4387fba9bbc8e6766837
Igor Smirnov <smirnoviv@rambler.ru>
2020-11-04 09:41:33 +0500

    Merge databases after restore

--- src/gather/restore.clj
+++ src/gather/restore.clj
@@ -18,7 +18,7 @@
       reme-dir (str data-dir "/metadata")
       ]
       (try
-        (println "Create database restore")
+        (println "Create database 'restore'")
         (ch/exec-query! st "CREATE DATABASE restore ENGINE = Ordinary")
         (println "Extracting data")
         (c/extract! (str (c/pwd) "/" file-name) data-dir)
@@ -28,6 +28,17 @@
         (println "Attaching tables")
         (doseq [table (c/ls data-dir)]
           (ch/exec-query! st (str "ATTACH TABLE restore." table)))
+        (println "Merge DB 'restore' to 'fx'")
+        (doseq [
+          [table parts] (ch/fetch-partitions st :db "restore")
+          part parts]
+          (println "Moving" table part)
+          (ch/exec-query! st (str
+            "ALTER TABLE restore." table
+            " MOVE PARTITION " part
+            " TO TABLE fx." table)))
+        (println "Remove database 'restore'")
+        (ch/exec-query! st "DROP DATABASE restore")
         (println "Completed")
       (catch Exception e
         (println "\nException:" (.getMessage e))))))
